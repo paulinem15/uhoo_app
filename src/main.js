a3ed9814cadbab873d3a2b07d40370066e5dbed3
Pauline Manolo <pauline@uhooair.com>
2019-03-25 16:07:04 +0800

    released first version

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
-const electron = require('electron')
+const {ipcMain, app, BrowserWindow} = require('electron');
 const path = require('path')
 const url = require('url')
-const autoUpdater = require('./auto-updater')
-if (require('electron-squirrel-startup')) electron.app.quit()
+// const autoUpdater = require('./auto-updater')
+const {autoUpdater} = require("electron-updater");
+// if (require('electron-squirrel-startup')) electron.app.quit()
 
-const app = electron.app
-const BrowserWindow = electron.BrowserWindow
+// const app = electron.app
+// const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
@@ -23,15 +24,19 @@ function createWindow () {
   }
 
   mainWindow.on('closed', () => {
-    mainWindow = null
+    app.quit()
   })
 
-  mainWindow.webContents.on('did-finish-load', () => {
-    autoUpdater.init(mainWindow)
-  })
 }
 
-app.on('ready', createWindow)
+app.on('ready', function(){
+  createWindow()
+  autoUpdater.checkForUpdates();
+})
+
+autoUpdater.on('update-downloaded', (info) => {
+    win.webContents.send('updateReady')
+});
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
